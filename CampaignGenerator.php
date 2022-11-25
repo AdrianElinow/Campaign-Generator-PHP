e6596eef9d4bb69b832d9a9ebd86b327bae6840c
Adrian Elinow <adrianelinow@gmail.com>
2022-11-25 13:45:09 -0500

    init commit
    skeleton framework

---
+++ CampaignGenerator.php
@@ -0,0 +1,32 @@
+
+<?php
+
+	class SimulaeNode{}
+
+	class NGINPHP{
+
+
+		function generate_element(){}
+
+		function generate_state(){}
+
+		function generate_mission(){}
+
+		function generate_event(){}
+
+		function choose_mission(){}
+
+		function user_choice(){}
+
+		function start(){}
+
+	}
+	
+
+	function main(){
+
+	}
+
+	main()
+
+?>
